8bdbb881730c98c5279396350db89a3b822b8754
Gladwin Del Rosario <gladelrosario12@gmail.com>
2024-02-29 04:42:55 +0800

    new fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Router basename={process.env.PUBLIC_URL || ''}>
+<Router basename="/">
   <App/>
 </Router>
 );
